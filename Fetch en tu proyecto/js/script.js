dcfd9bccd7b53600729dad523588f043b6f8ade9
Daniel <davillera77@gmail.com>
2022-08-22 21:07:12 -0500

    fetch en tu proyecto

---
+++ Fetch en tu proyecto/js/script.js
@@ -0,0 +1,38 @@
+
+document.getElementById("bandera0").src = `./banderas/${from.value}.png`
+document.getElementById("bandera1").src = `./banderas/${to.value}.png`
+
+importe = document.getElementById("importe")
+from = document.getElementById("from")
+to = document.getElementById("to")
+
+const btnCalcular = document.getElementById("btnCalcular");
+btnCalcular.addEventListener("click", () => calcular());
+
+
+
+
+function cambioBandera(){
+    document.getElementById("bandera0").src = `./banderas/${from.value}.png`
+    document.getElementById("bandera1").src = `./banderas/${to.value}.png`
+}
+
+
+
+function calcular(){
+    let myHeaders = new Headers();
+    myHeaders.append("apikey", "LS7OtHZzG4it51Zv4zZL5Uv0GpU6L2CO");
+
+    let requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+
+    localStorage.setItem("importe", importe.value);
+
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${importe.value}`, requestOptions)
+    .then(response => response.json())
+    .then((data) => console.log(data.result))
+    .catch(error => console.log('error', error))
+}
